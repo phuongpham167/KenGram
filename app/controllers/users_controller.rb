ff518f8aa45d96a42fa00e06b51569e17306a31e
Phuong Pham <phuongptt.uet@gmail.com>
2018-06-25 11:28:23 +0700

    finish signup account

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,60 @@
+class UsersController < ApplicationController
+  before_action :load_user, only: %i(show edit update destroy)
+
+  def index;  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = t ".success"
+      redirect_to @user
+    else
+      render :new
+    end
+  end
+
+  def show
+    @user = User.find_by id: params[:id]
+    return if @user
+    redirect_to root_url
+  end
+
+  def edit; end
+
+  def update
+    respond_to do |format|
+      if @user.update(user_params)
+        format.html { redirect_to @user, notice: t(".notice") }
+        format.json { render :show, status: :ok, location: @user }
+      else
+        format.html { render :edit }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def destroy
+    @user.destroy
+    respond_to do |format|
+      format.html { redirect_to users_url, notice: t(".notice") }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+    def load_user
+      @user = User.find_by id: params[:id]
+      return if @user
+      flash[:danger] = t ".danger"
+      redirect_to root_url
+    end
+
+    def user_params
+      params.require(:user).permit :name, :email, :password,
+       :password_confirmation
+    end
+end
